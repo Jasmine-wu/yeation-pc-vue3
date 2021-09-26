77b4af4d9639c9f78d3f37fb3db24fdd0feee811
jasmine <18674601207@163.com>
2021-09-26 09:12:11 +0800

    add interactive of home-category-menu

--- src/views/home/components/home-category-menu.vue
+++ src/views/home/components/home-category-menu.vue
@@ -2,7 +2,11 @@
   <div class="category-menu">
     <!-- 分类菜单 -->
     <ul class="menu-content">
-      <li v-for="category in menuList" :key="category.id">
+      <li
+        v-for="category in menuList"
+        :key="category.id"
+        @mouseenter="categoryId = category.id"
+      >
         <router-link :to="`/category/${category.id}`">{{
           category.name
         }}</router-link>
@@ -21,43 +25,50 @@
     <!-- 分类推荐弹层次 -->
     <div class="layer">
       <div class="layer-title">
-        <span>分类推荐</span>
+        <span
+          >{{
+            curRecoms && curRecoms.id === "brand" ? "品牌" : "分类"
+          }}推荐</span
+        >
         <span>根据您的购买记录或者浏览记录推荐</span>
       </div>
-      <ul class="layer-content">
-        <li v-for="(i, index) in 9" :key="index">
-          <img
-            class="left"
-            src="https://yanxuan-item.nosdn.127.net/5a115da8f2f6489d8c71925de69fe7b8.png"
-            alt=""
-          />
-          <div class="right">
-            <p class="name ellipsis-2">【定金购】严选零食大礼包（12件）</p>
-            <p class="desc ellipsis">超值组合装，满足馋嘴欲</p>
-            <p class="price"><i>¥</i>100.00</p>
-          </div>
+      <ul class="layer-content" v-if="curRecoms && curRecoms.goods">
+        <li v-for="goods in curRecoms.goods" :key="goods.id">
+          <router-link to="/">
+            <img class="left" :src="goods.picture" alt="" />
+            <div class="right">
+              <p class="name ellipsis-2">{{ goods.name }}</p>
+              <p class="desc ellipsis">{{ goods.desc }}</p>
+              <p class="price"><i>¥</i>{{ goods.price }}</p>
+            </div>
+          </router-link>
         </li>
       </ul>
     </div>
   </div>
 </template>
 <script>
-import { computed, reactive } from "vue";
+import { computed, reactive, ref } from "vue";
 import { useStore } from "vuex";
+import { findBrand } from "@/api/home.js";
+
 export default {
   name: "HomeCategoryMenu",
   setup() {
+    // 1.准备分类菜单数据
     // 左侧分类菜单的数据 = 9个一级类目>+2个二级分类+一个品牌>品牌推荐
     const store = useStore();
-    // console.log(store.state.category.categoryList); //这是的categoryList还是默认的数据
+    // console.log(store.state.category.categoryList); //这是的categoryList只有默认的数据
 
     const lastBrand = reactive({
       id: "brand",
       name: "品牌",
       children: [{ id: "brand-chilren", name: "品牌推荐" }],
+      goods: [],
     });
     // 如果获取确保categoryList是网络请求完毕后的categoryList-->计算属性
     const menuList = computed(() => {
+      // 1.1 9个一级类目>+2个二级分类
       const list = store.state.category.categoryList.map((category) => {
         return {
           id: category.id,
@@ -68,13 +79,46 @@ export default {
       });
       // 计算属性返回的数组其实是伪数组，用不了push
       // 因此要在这里push
+      // 1.2 一个品牌>品牌推荐
       list.push(lastBrand);
 
       return list;
     });
 
+    // 2. 准备分类推荐数据，用响应对象实现
+    // const curRecoms = reactive({
+    //   goods: {},
+    // });
+    // // 2.1 在鼠标经过enter菜单li时，记住当前一级分类id
+    // const addMouseEnterListener = (id) => {
+    //   // 2.2 根据一级分类id筛选分类推荐数据（在goods里）
+    //   const curCategory = menuList.value.find((category) => category.id === id);
+    //   curRecoms.goods = curCategory.goods && curCategory.goods;
+    // };
+    // return { menuList, addMouseEnterListener, curRecoms };
+
+    // 2. 用计算属性实现:更简洁
+    const categoryId = ref(0);
+    const curRecoms = computed(() => {
+      return menuList.value.find(
+        (category) => category.id === categoryId.value
+      );
+    });
+
+    // 3.单独处理品牌/品牌推荐栏的数据
+    findBrand().then((data) => {
+      lastBrand.goods =
+        data.result.length > 9 ? data.result.slice(0, 9) : data.result;
+    });
+
+    // 3.1 思考，为什么用.then获取，可以用await获取结果么？
+    // 最好不要！
+    // const data = await findBrand();
+    // lastBrand.goods =
+    //   data.result.length > 9 ? data.result.slice(0, 9) : data.result;
+
     // menuList.push(brand); //而不是这里
-    return { menuList };
+    return { menuList, categoryId, curRecoms };
   },
 };
 </script>
@@ -113,7 +157,10 @@ export default {
     width: 990px;
     padding: 0 15px;
     border: 1px solid @xtxColor;
+    border-left: none;
     background: rgba(255, 255, 255, 0.8);
+    // 默认隐藏
+    display: none;
 
     // 标题
     .layer-title {
@@ -137,7 +184,6 @@ export default {
       margin: 10px 0;
 
       li {
-        display: flex;
         width: 310px;
         height: 120px;
         padding: 10px;
@@ -149,7 +195,9 @@ export default {
         border: 1px solid #eee;
         border-radius: 4px;
         background: #fff;
-
+        a {
+          display: flex;
+        }
         .right {
           display: flex;
           flex-direction: column;
@@ -175,6 +223,12 @@ export default {
         height: 95px;
         margin-right: 5px;
       }
+    }
+  }
+  // 鼠标经过menu时，显示分类推荐弹层
+  &:hover {
+    .layer {
+      display: block;
     }
   }
 }
